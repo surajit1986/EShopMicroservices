a2584d2d297913ababf47516e75034b8198ee282
Surajit Ghosh <surajit.learning@gmail.com>
2026-01-10 11:07:36 +0530

    mediatr pattern added in building blocks

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, List<string> Category, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
